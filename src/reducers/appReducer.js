3c21700c0a7d57e7ed5d274826e44ea752821032
Logan Scott <logantscott@gmail.com>
2020-08-04 08:04:53 -0600

    appreducer test

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,9 +1,11 @@
 export const initialState = {
-
+  characters: []
 };
 
 export default function reducer(state, action) {
   switch(action.type) {
+    case 'SET_CHARACTERS':
+      return { ...state, characters: action.payload };
     default: 
       return state;
   }
